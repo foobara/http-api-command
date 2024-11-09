b482c6ccb730ee1fb5e6228421a8e3025dc72a92
Miles Georgi <azimux@gmail.com>
2024-11-08 19:22:46 -0800

    Create ruby project files

---
+++ spec/foobara/http_api_command_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Foobara::HttpApiCommand do
+  it "has a version number" do
+    expect(Foobara::HttpApiCommand::VERSION).to_not be_nil
+  end
+end
---
+++ version.rb
@@ -0,0 +1,5 @@
+module Foobara
+  module HttpApiCommand
+    VERSION = "0.0.1".freeze
+  end
+end
